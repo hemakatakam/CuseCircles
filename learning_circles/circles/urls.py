cb818f189f62f59ca9d41bcc740a02a79ed07044
hemakatakam <hkatakam@syr.edu>
2024-10-05 20:15:53 -0400

    Fixed signup bugs

--- learning_circles/circles/urls.py
+++ learning_circles/circles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create_circle, name='create_circle'),
     path('join/<int:circle_id>/', views.join_circle, name='join_circle'),
     path('exit/<int:circle_id>/', views.leave_circle, name='leave_circle'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
